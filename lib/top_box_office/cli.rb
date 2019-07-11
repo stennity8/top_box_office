3c2b7d0b00af4a51fa6a1ed33da51baaba6058bb
Shawn <shawntennity@gmail.com>
2019-07-11 18:13:40 -0400

    Set up initial output of movie list with scraped location and date

--- lib/top_box_office/cli.rb
+++ lib/top_box_office/cli.rb
@@ -3,18 +3,15 @@ class TopBoxOffice::CLI
     TopBoxOffice::Scraper.scrape_imdb
     bo_image
     puts "\nWelcome to Top Box Office!\n\n"
+
     # Scraper class runs and puts all movies into Movie Class(s)
-    binding.pry
-    # expected output to be something like below
-    puts "Here is a list of the top movies in the box office right now:"
-    puts "1. Spiderman"
-    puts "2. Toy Story 4"
-    puts "3. Yesterday"
+    print_heading
+
     # Get user input for box office data
-    puts "Which movie would you like to see the earnings for as of {weekend}?"
+    puts "\nWhich number on the list would you like to see the earnings for?"
     input = gets.strip.to_i
 
-    puts "Chosen movie box office earnings:"
+    #puts "Chosen movie box office earnings:"
   end
 
   def bo_image
@@ -34,5 +31,19 @@ class TopBoxOffice::CLI
     puts "  |    -  |    |- | -      -     |  |    | --      |"
     puts "  |_______|====|__|______________|__|====|_________|"
         
+  end
+
+  def print_heading
+    location = TopBoxOffice::Movie.all[0].location.split(" ").last[1,2]
+    heading_date = TopBoxOffice::Movie.all[0].date
+    
+    puts "\nThe Top Movies in the #{location} Box Office as of the #{heading_date} are:\n\n"
+
+    TopBoxOffice::Movie.all.each.with_index(1) do |movie, index|
+      title = movie.title
+      puts "#{index}. #{title}"
+    end
+    # binding.pry
+
   end
 end
