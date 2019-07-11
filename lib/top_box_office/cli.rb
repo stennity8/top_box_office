1977ef45c022d7f21a7e2572afa1b253161861d3
Shawn <shawntennity@gmail.com>
2019-07-11 15:48:16 -0400

    Added to CLI class and set up send method in Movie class

--- lib/top_box_office/cli.rb
+++ lib/top_box_office/cli.rb
@@ -1,5 +1,32 @@
 class TopBoxOffice::CLI
   def call
-    puts 'Welcome to Top Box Office!'
+    bo_image
+    puts "\nWelcome to Top Box Office!\n\n"
+    # Scraper class runs and puts all movies into Movie Class(s)
+    # expected output to be something like below
+    puts "Here is a list of the top movies in the box office right now:"
+    puts "1. Spiderman"
+    puts "2. Toy Story 4"
+    puts "3. Yesterday"
+    puts "Which movie would you like to see the earnings for as of {weekend}?"
+  end
+
+  def bo_image
+    puts ""
+    puts "               @-_________________-@"
+    puts "         @-_____|   NOW SHOWING   |_____-@"
+    puts "          |                             |"
+    puts "   _______|_____________________________|__________"
+    puts "  |________________________________________________|"
+    puts "  |               -                -               |"
+    puts "  |   -       -             -                    - |"
+    puts "  |        ____    ______________-   ____          |"
+    puts "  | -  -  |    |   |  TICKETS   |   |    | -       |"
+    puts "  |       |    |   |            |   |    |         |"
+    puts "  |  --   |____| - |_o___oo___o_| - |____|     -   |"
+    puts "  | -     |    |  |     --       |  |    |         |"
+    puts "  |    -  |    |- | -      -     |  |    | --      |"
+    puts "  |_______|====|__|______________|__|====|_________|"
+        
   end
 end
--- lib/top_box_office/movie.rb
+++ lib/top_box_office/movie.rb
@@ -1,3 +1,20 @@
 class TopBoxOffice::Movie
+  attr_accessor :title, :weekend, :gross, :weeks, :rated, :length, :genres, :release_date,
+                :rating, :review_number, :tag_line, :director, :stars 
+
+  @@all = []
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    @@all << self
+  end
+
+  def more_info(attributes)
+    attributes.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
 
 end
